91693909850d03e88bfa49d31e4f5cb8da0c6608
Bryan Stanley <bryan@invisionapp.com>
2020-12-14 17:16:21 -0500

    Testing env variables

--- index.php
+++ index.php
@@ -5,5 +5,5 @@
 <h1>First Auto Deploy That Worked!</h1>
 <h2>With ENV variables:</h2>
 <ul>
-  <li>Database Name: <?php if (isempty($db)) ?  $db ?></li>
+  <li>Database Name: <?php echo empty($db) ? 'NOT SET' : $db ?></li>
 </ul>
